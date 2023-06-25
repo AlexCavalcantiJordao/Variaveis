bca0fe803492622991e6cdecc95ac59bc737c723
AlexCavalcantiJordao <alex37705732@gmail.com>
2023-06-25 17:34:26 -0300

    build.xml build/classes/.netbeans_automatic_build build/classes/.netbeans_update_resources build/classes/variaveis/Variaveis.class manifest.mf nbproject/build-impl.xml nbproject/genfiles.properties nbproject/private/private.properties nbproject/private/private.xml nbproject/project.properties nbproject/project.xml src/variaveis/Variaveis.java

---
+++ src/variaveis/Variaveis.java
@@ -0,0 +1,14 @@
+package variaveis;
+
+public class Variaveis {
+
+    // Método prinipal da Classse;
+    public static void main(String[] args) {
+
+        // System.out.println("Texto Simples");
+        String nome = "Alex";
+        int idade = 26;
+        boolean casado = true;
+        System.out.println(" Seu nome é: " + nome + " .A sua idade é: " + idade + " .Seu estado Civil." + true);
+    }// Fim do Método;
+}// Fim da Classe;
